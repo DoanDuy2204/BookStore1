30223602a2c66d49d2f403798a17e525c814b6cf
Doan Duy <Doan Duy@DESKTOP-5E2K74F>
2019-01-25 22:58:52 +0700

    this commit 2019/01/25

--- src/main/resources/sql/book_storejsp_authorities.sql
+++ src/main/resources/sql/book_storejsp_authorities.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
--- Host: localhost    Database: book_store1
+-- Host: localhost    Database: bookstore
 -- ------------------------------------------------------
 -- Server version	8.0.13
 
